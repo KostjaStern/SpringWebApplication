f5957230f8bad849305e6f0b2403a0a44feb9ef7
KostjaStern <kostjastern@gmail.com>
2018-06-23 14:37:14 +0300

    maven warnings were fixed

--- mvn8.bat
+++ mvn8.bat
@@ -1,4 +1,4 @@
 set "JAVA_HOME=C:\Program Files\Java\jdk1.8.0_101"
 rem >mvn -DargLine="-Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2" package
 call mvn -DargLine="-Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2" clean package -DskipTests
-pause
+rem pause
